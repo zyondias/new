9eeb71908068794772c677c2b800db48bd0972ed
zyondias <zyon.dias@hotmail.com>
2013-03-28 11:23:46 -0300

    Modificação Da class Inscricao

--- app/Model/Inscricao.php
+++ app/Model/Inscricao.php
@@ -2,9 +2,16 @@
 
 class Inscricao extends AppModel
 {
-    /*public function find($type = 'all', $query = array('')) {
-        parent::find($type, $query);
-    }*/
+    //colocando em ordem Ascedente com base no nome
+    public $order = array('nome' => 'ASC');
+    //habiltando cache para query para armazenar o cache do servidoro ultimo resultado obtido de uma query executada assim ele não precisa executar a mesma query
+    public $cacheQueries = true;
+    //fazendo validações dos campos obrigatorios na base de dados
+    public $validate = array( 
+         'nome'=>array('rule'=>'notEmpty','message'=>'preencha o seu nome'),
+         'email'=>array('rule'=>'notEmpty','message'=>'Preencha o seu E-mail')
+        );
+
 
 }
 
